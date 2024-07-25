8a15d689b536ddf2b8d092f89c3deef34624efb5
Ashu1244 <97390191+Ashu1244@users.noreply.github.com>
2024-07-26 02:48:51 +0530

    Sign in page made
    Sign in page made successfully

--- servicedo/src/main.jsx
+++ servicedo/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import EmployeeRegister from './pages/EmployeeRegister/EmployeeRegister.jsx';
 import UserRegister from './pages/UserRegister/UserRegister.jsx';
+import Sign_in from './pages/Sign_in/Sign_in.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/' element={<App />} />
         <Route path='/employeeRegister' element={<EmployeeRegister />} />
         <Route path='/userRegister' element={<UserRegister />} />
+        <Route path='/Sign_in' element={<Sign_in />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
